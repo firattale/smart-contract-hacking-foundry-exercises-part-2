8e135cad45a38cb4f393c75d00c971389cd55f06
firatstory.eth <talefirat@gmail.com>
2024-04-15 15:27:59 +0200

    feat: :sparkles: add Chocolate.sol and TestDEX1.t.sol

--- src/dex-1/Chocolate.sol
+++ src/dex-1/Chocolate.sol
@@ -26,15 +26,27 @@ contract Chocolate is ERC20, Ownable {
     using Address for address payable;
 
     IUniswapV2Router02 public uniswapV2Router;
+    IUniswapV2Factory public uniswapV2Factory;
+    IUniswapV2Pair public uniswapV2Pair;
+    IWETH public weth;
 
-    address public weth;
-    address public uniswapV2Pair;
+    address public uniswapV2PairAddress;
+    address public constant UNISWAP_V2_ROUTER_ADDRESS = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address public constant UNISWAP_V2_FACTORY_ADDRESS = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+    address public constant WETH_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     constructor(uint256 _initialMint) ERC20("Chocolate Token", "Choc") {
         // TODO: Mint tokens to owner
+        _mint(owner(), _initialMint);
         // TODO: SET Uniswap Router Contract
+        uniswapV2Router = IUniswapV2Router02(UNISWAP_V2_ROUTER_ADDRESS);
         // TODO: Set WETH (get it from the router)
+        weth = IWETH(WETH_ADDRESS);
         // TODO: Create a uniswap Pair with WETH, and store it in the contract
+        uniswapV2Factory = IUniswapV2Factory(UNISWAP_V2_FACTORY_ADDRESS);
+        uniswapV2PairAddress = uniswapV2Factory.createPair(address(this), WETH_ADDRESS);
+
+        uniswapV2Pair = IUniswapV2Pair(uniswapV2PairAddress);
     }
 
     /*
@@ -42,14 +54,19 @@ contract Chocolate is ERC20, Ownable {
         @dev payable, received Native ETH and converts it to WETH
         @dev lp tokens are sent to contract owner
     */
-    function addChocolateLiquidity(
-        uint256 _tokenAmount
-    ) external payable onlyOwner {
+    function addChocolateLiquidity(uint256 _tokenAmount) external payable onlyOwner {
         // TODO: Transfer the tokens from the sender to the contract
         // Sender should approve the contract spending the chocolate tokens
+        this.transferFrom(owner(), address(this), _tokenAmount);
         // TODO: Convert ETH to WETH
+        weth.deposit{value: msg.value}();
         // TODO: Approve the router to spend the tokens
-        // TODO: Add the liquidity, using the router, send lp tokens to the contract owner
+        weth.approve(address(uniswapV2Router), msg.value);
+        this.approve(address(uniswapV2Router), _tokenAmount);
+        // // TODO: Add the liquidity, using the router, send lp tokens to the contract owner
+        uniswapV2Router.addLiquidity(
+            address(this), WETH_ADDRESS, _tokenAmount, msg.value, _tokenAmount, msg.value, owner(), block.timestamp
+        );
     }
 
     /*
@@ -57,13 +74,14 @@ contract Chocolate is ERC20, Ownable {
         @dev received `_lpTokensToRemove`, removes the liquidity
         @dev and sends the tokens to the contract owner
     */
-    function removeChocolateLiquidity(
-        uint256 _lpTokensToRemove
-    ) external onlyOwner {
+    function removeChocolateLiquidity(uint256 _lpTokensToRemove) external onlyOwner {
         // TODO: Transfer the lp tokens from the sender to the contract
         // Sender should approve token spending for the contract
+        uniswapV2Pair.transferFrom(owner(), address(this), _lpTokensToRemove);
         // TODO: Approve the router to spend the tokens
+        uniswapV2Pair.approve(address(uniswapV2Router), _lpTokensToRemove);
         // TODO: Remove the liquiduity using the router, send tokens to the owner
+        uniswapV2Router.removeLiquidity(address(this), WETH_ADDRESS, _lpTokensToRemove, 1, 1, owner(), block.timestamp);
     }
 
     /*
@@ -71,26 +89,35 @@ contract Chocolate is ERC20, Ownable {
         @dev received `_lpTokensToRemove`, removes the liquidity
         @dev and sends the tokens to the contract user that swapped
     */
-    function swapChocolates(
-        address _tokenIn,
-        uint256 _amountIn
-    ) public payable {
+    function swapChocolates(address _tokenIn, uint256 _amountIn) public payable {
         // TODO: Implement a dynamic function to swap Chocolate to ETH or ETH to Chocolate
 
+        address[] memory path = new address[](2);
         if (_tokenIn == address(this)) {
             // TODO: Revert if the user sent ETH
+            require(msg.value == 0, "no ETH transfer allowed");
             // TODO: Set the path array
+            path[0] = address(this);
+            path[1] = address(weth);
             // TODO: Transfer the chocolate tokens from the sender to this contract
             // TODO: Approve the router to spend the chocolate tokens
-        } else if (_tokenIn == weth) {
+            this.transferFrom(msg.sender, address(this), _amountIn);
+            this.approve(address(uniswapV2Router), _amountIn);
+        } else if (_tokenIn == WETH_ADDRESS) {
             // TODO: Make sure msg.value equals _amountIn
+            require(msg.value == _amountIn, "You need to send enough ETH");
             // TODO: Convert ETH to WETH
+            weth.deposit{value: msg.value}();
             // TODO: Set the path array
+            path[0] = address(weth);
+            path[1] = address(this);
             // TODO: Approve the router to spend the WETH
+            weth.approve(address(uniswapV2Router), msg.value);
         } else {
             revert("wrong token");
         }
 
         // TODO: Execute the swap, send the tokens (chocolate / weth) directly to the user (msg.sender)
+        uniswapV2Router.swapExactTokensForTokens(_amountIn, 0, path, msg.sender, block.timestamp);
     }
 }
--- test/dex-1/TestDEX1.t.sol
+++ test/dex-1/TestDEX1.t.sol
@@ -11,27 +11,28 @@ import {IUniswapV2Pair} from "src/interfaces/IUniswapV2.sol";
  * @dev run "forge test -vvv --fork-url mainnet --fork-block-number 15969633 --match-contract DEX1"
  */
 contract TestDEX1 is Test {
-    Chocolate chocolate;
-    IUniswapV2Pair pair;
+    Chocolate public chocolate;
+    IUniswapV2Pair public pair;
 
-    address constant WETH_ADDRESS = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    address constant RICH_SIGNER = address(0x8EB8a3b98659Cce290402893d0123abb75E3ab28);
+    address public constant WETH_ADDRESS = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 
-    uint128 constant ETH_BALANCE = 300 ether;
-    uint128 constant INITIAL_MINT = 1000000 ether;
-    uint128 constant INITIAL_LIQUIDITY = 100000 ether;
-    uint128 constant ETH_IN_LIQUIDITY = 100 ether;
+    uint128 public constant ETH_BALANCE = 300 ether;
+    uint128 public constant INITIAL_MINT = 1_000_000 ether;
+    uint128 public constant INITIAL_LIQUIDITY = 100_000 ether;
+    uint128 public constant ETH_IN_LIQUIDITY = 100 ether;
 
-    uint128 constant TEN_ETH = 10 ether;
-    uint128 constant HUNDRED_CHOCOLATES = 100 ether;
+    uint128 public constant TEN_ETH = 10 ether;
+    uint128 public constant HUNDRED_CHOCOLATES = 100 ether;
 
-    address deployer = makeAddr("deployer");
-    address user = makeAddr("user");
+    address public deployer = makeAddr("deployer");
+    address public user = makeAddr("user");
 
-    IERC20 weth = IERC20(WETH_ADDRESS);
+    IERC20 public weth = IERC20(WETH_ADDRESS);
 
     function setUp() public {
         vm.label(WETH_ADDRESS, "WETH");
+        vm.label(user, "USER");
+        vm.label(deployer, "DEPLOYER");
         vm.deal(deployer, 100 ether);
         vm.deal(user, 100 ether);
     }
@@ -39,31 +40,73 @@ contract TestDEX1 is Test {
     function testDex1() public {
         // --------------- Deploy ---------------
         // TODO: Deploy your smart contract to `chocolate`, mint 1,000,000 tokens to deployer
-        
+        vm.startPrank(deployer);
+
+        chocolate = new Chocolate(INITIAL_MINT);
+
         // TODO: Print newly created pair address and store pair contract to `this.pair`
+        console.log("Pair address", chocolate.uniswapV2PairAddress());
+        pair = IUniswapV2Pair(chocolate.uniswapV2PairAddress());
 
         // --------------- Add Liquidity ---------------
         // TODO: Add liquidity of 100,000 tokens and 100 ETH (1 token = 0.001 ETH)
-        
+        chocolate.approve(address(chocolate), INITIAL_LIQUIDITY);
+        // weth.approve(address(chocolate), ETH_IN_LIQUIDITY);
+        chocolate.addChocolateLiquidity{value: ETH_IN_LIQUIDITY}(INITIAL_LIQUIDITY);
+
         // TODO: Print the amount of LP tokens that the deployer owns
+        console.log("Balance of LP tokens", pair.balanceOf(deployer));
+
+        vm.stopPrank();
 
         // --------------- Swap ---------------
         // TODO: From user: Swap 10 ETH to Chocolate
 
-        // TODO: Make sure user received the chocolates (greater amount than before)
+        vm.startPrank(user);
         uint256 userChocBalance = chocolate.balanceOf(user);
-        
-        // TODO: From user: Swap 100 Chocolates to ETH
-        uint256 userInitialWethBalance = weth.balanceOf(user);
-        
-        // TODO: Make sure user received the WETH (greater amount than before)
+        console.log("Initial Choc Balance", userChocBalance);
+        chocolate.swapChocolates{value: TEN_ETH}(WETH_ADDRESS, TEN_ETH);
 
+        // TODO: Make sure user received the chocolates (greater amount than before)
+        userChocBalance = chocolate.balanceOf(user);
+        console.log("After Swap Choc Balance", userChocBalance);
+
+        assertGt(userChocBalance, 0);
+
+        // TODO: From user: Swap 100 Chocolates to ETH
+        uint256 userWethBalance = weth.balanceOf(user);
+        console.log("User Initial Weth Balance", userWethBalance);
+
+        chocolate.approve(address(chocolate), HUNDRED_CHOCOLATES);
+
+        chocolate.swapChocolates(address(chocolate), HUNDRED_CHOCOLATES);
+
+        // TODO: Make sure user received the WETH (greater amount than before)
+        userWethBalance = weth.balanceOf(user);
+        console.log("User After swap Weth Balance", userWethBalance);
+        assertGt(userWethBalance, 0);
+
+        vm.stopPrank();
         // --------------- Remove Liquidity ---------------
 
         // TODO: Remove 50% of deployer's liquidity
+        vm.startPrank(deployer);
+        uint256 deployerPairBalance = pair.balanceOf(deployer);
+        uint256 deployerInitChocBalance = chocolate.balanceOf(deployer);
+        uint256 deployerInitWethBalance = weth.balanceOf(deployer);
+
+        uint256 lpTokensToRemove = deployerPairBalance / 2;
+
+        pair.approve(address(chocolate), lpTokensToRemove);
+
+        chocolate.removeChocolateLiquidity(lpTokensToRemove);
 
         // TODO: Make sure deployer owns 50% of the LP tokens (leftovers)
 
+        assertEq(pair.balanceOf(deployer), lpTokensToRemove);
+
         // TODO: Make sure deployer got chocolate and weth back (greater amount than before)
+        assertGt(chocolate.balanceOf(deployer), deployerInitChocBalance);
+        assertGt(weth.balanceOf(deployer), deployerInitWethBalance);
     }
 }
