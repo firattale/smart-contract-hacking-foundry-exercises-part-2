62518d9e1004a90b1562e1086b16af567d1e543a
firatstory.eth <talefirat@gmail.com>
2024-04-26 15:38:41 +0200

    refactor: :recycle: add sepolia TestSOCD2 tests

--- test/sensitive-on-chain-data-2/TestSOCD2.t.sol
+++ test/sensitive-on-chain-data-2/TestSOCD2.t.sol
@@ -6,22 +6,30 @@ import "forge-std/console.sol";
 import "./ISecretDoor.sol";
 
 /**
- * @dev run "forge test -vvv --fork-url goerli --fork-block-number 8660077 --match-contract SOCD2"
+ * @dev run "forge test -vvv --fork-url sepolia --fork-block-number 5781872 --match-contract SOCD2"
  */
 contract TestSOCD2 is Test {
-    address constant SECRET_DOOR = address(0x148f340701D3Ff95c7aA0491f5497709861Ca27D);
+    address constant SECRET_DOOR = address(0x5f83683A4DB0d73CA94046A85409097e459dE9de);
     address muggle = makeAddr("muggle");
 
     function testContract() public {
-        /** SETUP, DON'T CHANGE */
-
+        /**
+         * SETUP, DON'T CHANGE
+         */
         ISecretDoor secretDoor = ISecretDoor(SECRET_DOOR);
         assertTrue(secretDoor.isLocked());
 
-        /** CODE YOUR SOLUTION HERE */
-        
+        /**
+         * CODE YOUR SOLUTION HERE
+         */
+        bytes32 key = hex"454e5f425559554b5f46454e4552424148434500000000000000000000000000";
+        secretDoor.unlockDoor(key);
 
-        /** SUCCESS CONDITIONS */
+        // assertTrue(secretDoor.unlockDoor(bytes32(abi.encodePacked("EN_BUYUK_FENERBAHCE"))));
+
+        /**
+         * SUCCESS CONDITIONS
+         */
         assertFalse(secretDoor.isLocked());
     }
 }
