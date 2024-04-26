5403d7d776ee4b14f2a3d76ef43fbacba770f91f
firatstory.eth <talefirat@gmail.com>
2024-04-26 15:42:55 +0200

    add explanation to TestSOCD2

--- test/sensitive-on-chain-data-2/TestSOCD2.t.sol
+++ test/sensitive-on-chain-data-2/TestSOCD2.t.sol
@@ -8,6 +8,14 @@ import "./ISecretDoor.sol";
 /**
  * @dev run "forge test -vvv --fork-url sepolia --fork-block-number 5781872 --match-contract SOCD2"
  */
+// storage 0 is address private _owner from Ownable
+// storage 1 is uint256 private _status from ReentrancyGuard
+// storage 2 is  bool public isLocked and uint8 private doorNumber from contract combined
+// storage 3 is bytes32 private doorOwnerName from contract
+// storage 4 is bytes32 private secretSpell from contract
+
+//  cast storage 0x5f83683A4DB0d73CA94046A85409097e459dE9de --rpc-url sepolia 4
+
 contract TestSOCD2 is Test {
     address constant SECRET_DOOR = address(0x5f83683A4DB0d73CA94046A85409097e459dE9de);
     address muggle = makeAddr("muggle");
